ea2094d4420ad4df9024e0e4d089cf3432d67a81
Andrei Poenaru <andreigpoenaru@gmail.com>
2023-06-29 21:08:41 +0200

    Add Hook for attaching a fake api key to requests

--- zaptecbilling.py
+++ zaptecbilling.py
@@ -1,11 +1,22 @@
 from zaptecswagger import new_client, Configuration
 from swagger_codegen_fork.api.adapter.requests import RequestsAdapter
+from swagger_codegen_fork.api.configuration import Hook
+from swagger_codegen_fork.api.request import ApiRequest
+
+class ApiKeyHook:
+    api_key = "fake_api_key"
+
+    def add_api_key(self, request: ApiRequest) -> ApiRequest:
+        return request.clone(headers=dict(request.headers, **{'ApiKey': self.api_key}))
 
 
 def main():
-    client = new_client(RequestsAdapter(), Configuration(host="https://api.zaptec.com/"))
-    # client.charge_history.post__api_chargehistory_installationreport({})
+    client = new_client(RequestsAdapter(),
+                        Configuration(
+                            host="https://api.zaptec.com/",
+                            hooks={Hook.request: [ApiKeyHook().add_api_key]}))
     client.charge_history.get__api_chargehistory()
+    # client.charge_history.post__api_chargehistory_installationreport({})
 
 
 if __name__ == '__main__':
